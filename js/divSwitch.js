58148d24bfd1f4af9a7a5cd4650840d97104774d
Abhijit8 <57721823+Abhijit8@users.noreply.github.com>
2019-11-14 13:13:57 +0530

    First Push
    lol

---
+++ js/divSwitch.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+	var divNames = ["ARTS", "PHOTOS", "DOORMAT", "VIDEOS"];
+	var divIndex = 0;
+  $('#right-arrow').click(function() {
+	var currentSlide = $('.slide.active');
+	var nextSlide = currentSlide.next();
+	  
+	if(divIndex == 3){
+		divIndex = -1;
+	} 
+	var currentDivName = divIndex++;
+	document.getElementById("divName").innerHTML = divNames[divIndex];
+
+	
+	currentSlide.fadeOut(300).removeClass('active');
+	nextSlide.fadeIn(300).addClass('active');
+	
+	if(nextSlide.length == 0){
+		$('.slide').first().fadeIn(300).addClass('active');
+	}
+  });
+	
+  $('#left-arrow').click(function() {
+	var currentSlide = $('.slide.active');
+	var prevSlide = currentSlide.prev();
+	  
+	if(divIndex == 0){
+		divIndex = 4;
+	}
+	var currentDivName = divIndex--;
+	document.getElementById("divName").innerHTML = divNames[divIndex];
+	
+	  
+	  
+	currentSlide.fadeOut(300).removeClass('active');
+	prevSlide.fadeIn(300).addClass('active');
+	
+	if(prevSlide.length == 0){
+		$('.slide').last().fadeIn(300).addClass('active');
+	}
+  });
+});
